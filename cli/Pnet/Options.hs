fa52250e4427dc0910157cd3d29991fadb2bb870
indiscrete_void <indiscrete.void@gmail.com>
2024-04-06 21:50:18 +0200

    define CLI options

---
+++ cli/Pnet/Options.hs
@@ -0,0 +1,52 @@
+module Pnet.Options (parse) where
+
+import Options.Applicative
+
+newtype Options = Options Command
+
+data Transport
+  = Stdio
+  | Process !String
+
+data Command
+  = Ls
+  | Connect !String !Transport
+  | Tunnel !String !Transport
+
+parse :: IO Options
+parse = execParser parserInfo
+
+parserInfo :: ParserInfo Options
+parserInfo =
+  info
+    (helper <*> opts)
+    ( fullDesc
+        <> progDesc "Mananges pnet daemon, creates bidirectional link between it and the outer world"
+        <> header "Pandora Network manager"
+    )
+
+opts :: Parser Options
+opts =
+  Options
+    <$> hsubparser
+      ( command "ls" (info lsOpts $ progDesc "List nodes connected to daemon")
+          <> command "connect" (info connectOpts $ progDesc "Introduce a new node to daemon")
+          <> command "tunnel" (info tunnelOpts $ progDesc "Provide transport for application layer")
+      )
+
+lsOpts :: Parser Command
+lsOpts = pure Ls
+
+connectOpts :: Parser Command
+connectOpts = Connect <$> argument str (metavar "ID") <*> argument transport (metavar "TRANSPORT")
+
+tunnelOpts :: Parser Command
+tunnelOpts = Tunnel <$> argument str (metavar "ID") <*> argument transport (metavar "TRANSPORT")
+
+transport :: ReadM Transport
+transport = do
+  arg <- str
+  pure
+    if arg == "-"
+      then Stdio
+      else Process arg
