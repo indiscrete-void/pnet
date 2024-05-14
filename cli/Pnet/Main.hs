7e6b5a4f1290bf62abfacf378f79cfdbd3e6202c
indiscrete_void <indiscrete.void@gmail.com>
2024-05-14 20:12:17 +0200

    simplify pnet cli

--- cli/Pnet/Main.hs
+++ cli/Pnet/Main.hs
@@ -7,12 +7,11 @@ import Polysemy.Extra.Async
 import Polysemy.Extra.Trace
 import Polysemy.Fail
 import Polysemy.Serialize
-import Polysemy.Tagged
 import Polysemy.Trace
 import Polysemy.Transport
 import System.IO
 
-connectNodeStreams :: (Member ByteInputWithEOF r, Member ByteOutput r, Member Fail r, Member (InputWithEOF NodeToManagerMessage) r, Member (Output ManagerToNodeMessage) r, Member (Tagged 'Stdio Close) r, Member Async r, Member Trace r) => Transport -> Sem r ()
+connectNodeStreams :: (Member ByteInputWithEOF r, Member ByteOutput r, Member Fail r, Member (InputWithEOF NodeToManagerMessage) r, Member (Output ManagerToNodeMessage) r, Member Close r, Member Async r, Member Trace r) => Transport -> Sem r ()
 connectNodeStreams Stdio = async_ nodeToDaemon >> daemonToNode
   where
     nodeToDaemon = transferStream (msg . Just) (msg Nothing)
@@ -20,22 +19,20 @@ connectNodeStreams Stdio = async_ nodeToDaemon >> daemonToNode
         msg = ManagerNodeData . TunnelMessage
     daemonToNode = handle go
       where
-        go (DaemonNodeData (TunnelMessage maybeStr)) = tag @'Stdio @Close (maybe close output maybeStr)
+        go (DaemonNodeData (TunnelMessage maybeStr)) = maybe close output maybeStr
         go _ = _
 connectNodeStreams _ = _
 
-pnet :: (Member ByteInputWithEOF r, Member ByteOutput r, Member (InputWithEOF NodeToManagerMessage) r, Member (Output ManagerToNodeMessage) r, Member Close r, Member Fail r, Member Trace r, Member (Tagged 'Stdio Close) r, Member Async r) => Command -> Sem r ()
-pnet command = go command >> close
-  where
-    go Ls = output ListNodes >> (inputOrFail @NodeToManagerMessage >>= traceTagged "Ls" . show)
-    go (Connect transport nodeID) = output (ConnectNode transport nodeID) >> connectNodeStreams transport
-    go _ = _
+pnet :: (Member ByteInputWithEOF r, Member ByteOutput r, Member (InputWithEOF NodeToManagerMessage) r, Member (Output ManagerToNodeMessage) r, Member Fail r, Member Trace r, Member Close r, Member Async r) => Command -> Sem r ()
+pnet Ls = output ListNodes >> (inputOrFail @NodeToManagerMessage >>= traceTagged "Ls" . show)
+pnet (Connect transport nodeID) = output (ConnectNode transport nodeID) >> connectNodeStreams transport
+pnet _ = _
 
 main :: IO ()
 main =
   let run h = runFinal . asyncToIOFinal . embedToFinal @IO . failToEmbed @IO . traceToStderrBuffered . runTransport h . runStdio
-      runStdio = outputToIO stdout . inputToIO bufferSize stdin . (closeToIO stdout . untag @'Stdio @Close)
-      runTransport h = inputToIO bufferSize h . outputToIO h . closeToIO h . runUnserialized
+      runStdio = outputToIO stdout . inputToIO bufferSize stdin . closeToIO stdout
+      runTransport h = inputToIO bufferSize h . outputToIO h . runUnserialized
       runUnserialized = runDecoder . deserializeInput @NodeToManagerMessage . serializeOutput @ManagerToNodeMessage
    in withPnetSocket \sock -> do
         (Options command maybeSocketPath) <- parse
