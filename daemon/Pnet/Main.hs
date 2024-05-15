58a83ce9018271f6c6baa1057c791fdeb4005da2
indiscrete_void <indiscrete.void@gmail.com>
2024-05-15 03:40:05 +0200

    simplify networking

--- daemon/Pnet/Main.hs
+++ daemon/Pnet/Main.hs
@@ -1,7 +1,7 @@
 import Control.Monad
 import Network.Socket (bind, listen)
 import Pnet
-import Pnet.Networking
+import Pnet.Node
 import Pnet.Options
 import Polysemy hiding (run, send)
 import Polysemy.Async
@@ -15,53 +15,25 @@ import Polysemy.Transport
 import System.Exit
 import System.Posix
 import Text.Printf qualified as Text
-import Transport.Maybe
-
-handleNodeMsgs :: (Member (Input NodeToNodeMessage) r, Member (Output NodeToNodeMessage) r, Member Trace r) => Sem r ()
-handleNodeMsgs = forever $ input >>= go
-  where
-    go Ping = traceTagged "Ping" "Pong" >> output Pong
-    go Pong = traceTagged "Pong" "doing nothing"
-    go _ = _
-
-pnetnd :: (Member (Input NodeToNodeMessage) r, Member (Output NodeToNodeMessage) r, Member Trace r) => Sem r ()
-pnetnd = trace "sending Ping" >> output Ping >> handleNodeMsgs
 
 type State = [NodeID]
 
 initialState :: State
 initialState = []
 
-daemonNode :: (Member Fail r, Member (Socket ManagerToNodeMessage NodeToManagerMessage s) r, Member Decoder r, Member Trace r) => s -> Node (Sem r) NodeToNodeMessage NodeToNodeMessage
-daemonNode s =
-  unserializeNode $
-    Node
-      { nodeSend = send . DaemonNodeData . TunnelMessage . Just,
-        nodeRecv =
-          let managerNodeData (ManagerNodeData (TunnelMessage maybeStr)) = maybeStr
-              managerNodeData _ = _
-           in ((recv >>= maybeFailEOF) >>= maybeFailEOF . managerNodeData)
-      }
-  where
-    (Node send recv) = sockNode s
-
-handleManagerMsgs :: (Member Trace r, Members (SocketEffects ManagerToNodeMessage NodeToManagerMessage) r, Member (Socket ManagerToNodeMessage NodeToManagerMessage s) r, Member Fail r, Member Decoder r, Member (AtomicState State) r) => s -> Sem r ()
-handleManagerMsgs s = handle go >> close
+pnetd :: (Member Trace r, Member (Socket ManagerToNodeMessage NodeToManagerMessage s) r, Member (AtomicState State) r, Member Fail r, Member Decoder r, Member Async r) => Sem r ()
+pnetd = handleClient $ flip ioToSock (handle go >> close)
   where
     go ListNodes = do
       nodeList <- atomicGet
       traceTagged "ListNodes" (Text.printf "responding with `%s`" (show nodeList))
       output (NodeList nodeList)
     go (ConnectNode transport maybeNodeID) = do
-      let connectedDaemonNode = daemonNode s
       case maybeNodeID of
         Just nodeID -> atomicModify' (nodeID :) >> traceTagged "NodeAvailability" (Text.printf "%s connected over `%s`" (show nodeID) (show transport))
         Nothing -> traceTagged "NodeAvailability" (Text.printf "unknown node connected over `%s`" (show transport))
-      nodeToIO connectedDaemonNode pnetnd
+      mn2nn pnetnd
     go _ = _
-
-pnetd :: (Member Trace r, Member (Socket ManagerToNodeMessage NodeToManagerMessage s) r, Member (AtomicState State) r, Member Fail r, Member Decoder r, Member Async r) => Sem r ()
-pnetd = handleClient \s -> ioToSock s (handleManagerMsgs s)
 
 forkIf :: Bool -> IO () -> IO ()
 forkIf True m = forkProcess m >> exitSuccess
---
+++ daemon/Pnet/Node.hs
@@ -0,0 +1,58 @@
+module Pnet.Node
+  ( IpchainsMessage (..),
+    NodeToNodeMessage (..),
+    pnetnd,
+    mn2nn,
+  )
+where
+
+import Data.ByteString
+import Data.Serialize
+import GHC.Generics
+import Pnet
+import Polysemy hiding (send)
+import Polysemy.Extra.Trace
+import Polysemy.Fail
+import Polysemy.Output
+import Polysemy.Serialize
+import Polysemy.Socket
+import Polysemy.Trace
+import Polysemy.Transport
+import Transport.Maybe
+
+data IpchainsMessage = IpchainsMessage
+  { ipchainsMessageSrc :: NodeID,
+    ipchainsMessageDst :: NodeID,
+    ipchainsMessageData :: ByteString
+  }
+  deriving stock (Show, Generic)
+
+data NodeToNodeMessage where
+  Ipchains :: IpchainsMessage -> NodeToNodeMessage
+  Ping :: NodeToNodeMessage
+  Pong :: NodeToNodeMessage
+  deriving stock (Show, Generic)
+
+mn2nn :: (Member (InputWithEOF ManagerToNodeMessage) r, Member (Output NodeToManagerMessage) r, Member Decoder r, Member Fail r) => InterpretersFor (InputWithEOF NodeToNodeMessage ': Output NodeToNodeMessage ': '[]) r
+mn2nn = o2o . i2i
+  where
+    i2i :: (Member (InputWithEOF ManagerToNodeMessage) r, Member Decoder r, Member Fail r) => InterpreterFor (InputWithEOF NodeToNodeMessage) r
+    i2i = interpret \Input ->
+      let managerNodeData (ManagerNodeData (TunnelMessage maybeStr)) = maybeStr
+          managerNodeData _ = _
+       in Just <$> deserializeFrom (inputOrFail >>= maybeFailEOF . managerNodeData)
+    o2o :: (Member (Output NodeToManagerMessage) r) => InterpreterFor (Output NodeToNodeMessage) r
+    o2o = interpret \(Output msg) -> output . DaemonNodeData . TunnelMessage . Just $ serialize msg
+
+handleNodeMsgs :: (Members (SocketEffects NodeToNodeMessage NodeToNodeMessage) r, Member Trace r) => Sem r ()
+handleNodeMsgs = handle \case
+  Ping -> traceTagged "Ping" "Pong" >> output Pong
+  Pong -> traceTagged "Pong" "doing nothing"
+  _ -> _
+
+pnetnd :: (Members (SocketEffects NodeToNodeMessage NodeToNodeMessage) r, Member Trace r) => Sem r ()
+pnetnd = trace "sending Ping" >> output Ping >> handleNodeMsgs
+
+instance Serialize IpchainsMessage
+
+instance Serialize NodeToNodeMessage
