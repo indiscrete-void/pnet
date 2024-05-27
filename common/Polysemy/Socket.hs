c580d4cf4b272b7a66eb819dfd7e3c2c8897f075
indiscrete_void <indiscrete.void@gmail.com>
2024-05-27 05:53:36 +0200

    revert "shrink socket in function names"
    this reverts commit 012a3a0959e941e4460a9e28e64c251f1d5174bc.

--- common/Polysemy/Socket.hs
+++ common/Polysemy/Socket.hs
@@ -1,4 +1,4 @@
-module Polysemy.Socket (SocketEffects, inputToSock, outputToSock, closeToSock) where
+module Polysemy.Socket (SocketEffects, inputToSocket, outputToSocket, closeToSocket) where
 
 import Control.Monad
 import Network.Socket qualified as IO
@@ -9,11 +9,11 @@ import Transport.Maybe
 
 type SocketEffects i o = InputWithEOF i ': Output o ': Close ': '[]
 
-inputToSock :: (Member (Embed IO) r) => Int -> IO.Socket -> InterpreterFor ByteInputWithEOF r
-inputToSock bufferSize s = interpret \Input -> embed $ eofToNothing <$> IO.recv s bufferSize
+inputToSocket :: (Member (Embed IO) r) => Int -> IO.Socket -> InterpreterFor ByteInputWithEOF r
+inputToSocket bufferSize s = interpret \Input -> embed $ eofToNothing <$> IO.recv s bufferSize
 
-outputToSock :: (Member (Embed IO) r) => IO.Socket -> InterpreterFor ByteOutput r
-outputToSock s = interpret \(Output str) -> embed . void $ IO.send s str
+outputToSocket :: (Member (Embed IO) r) => IO.Socket -> InterpreterFor ByteOutput r
+outputToSocket s = interpret \(Output str) -> embed . void $ IO.send s str
 
-closeToSock :: (Member (Embed IO) r) => Int -> IO.Socket -> InterpreterFor Close r
-closeToSock timeout s = interpret \Close -> embed $ IO.gracefulClose s timeout
+closeToSocket :: (Member (Embed IO) r) => Int -> IO.Socket -> InterpreterFor Close r
+closeToSocket timeout s = interpret \Close -> embed $ IO.gracefulClose s timeout
