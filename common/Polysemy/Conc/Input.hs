e64f3ca37ecf53058b74a42cae8c9fe8c3be80da
indiscrete_void <indiscrete.void@gmail.com>
2024-06-12 09:08:36 +0200

    implement inputConc

--- common/Polysemy/Conc/Input.hs
+++ common/Polysemy/Conc/Input.hs
@@ -1,10 +1,25 @@
 module Polysemy.Conc.Input (inputConc, runInputConc) where
 
+import Data.List.Extra
 import Polysemy
+import Polysemy.Conc
+import Polysemy.State
 import Polysemy.Transport
 
-inputConc :: (Member (Input i) r) => Sem r i
-inputConc = _
+type ConcurrentInput i = '[Input i, State [i], State Int, Lock]
 
-runInputConc :: (Member (Input i) r) => InterpreterFor (Input i) r
-runInputConc = interpret \case Input -> inputConc
+inputConc :: (Members (ConcurrentInput i) r) => Sem r i
+inputConc = lockOr (lock goRead) goWrite <* modify (+ 1)
+  where
+    goWrite = do
+      i <- input
+      modify (<> [i])
+      pure i
+    goRead = do
+      buf <- get
+      index <- get
+      maybe inputConc pure $
+        buf !? index
+
+runInputConc :: (Members '[Input i, State [i], Lock] r) => InterpretersFor '[Input i, State Int] r
+runInputConc = evalState 0 . interpret \case Input -> inputConc
--- tests/Main.hs
+++ tests/Main.hs
@@ -1,4 +1,14 @@
+import Control.Monad
+import Control.Monad.Extra
 import Pnet.Routing
+import Polysemy.Async
+import Polysemy.Conc
+import Polysemy.Conc.Input
+import Polysemy.Final
+import Polysemy.Input
+import Polysemy.Output
+import Polysemy.Resource
+import Polysemy.State
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -13,8 +23,24 @@ testR2 =
             r2 SendTo 0 (RouteTo 1 msg) @?= SendTo 1 (RoutedFrom 0 msg)
     ]
 
+testConcInput :: TestTree
+testConcInput =
+  testGroup
+    "Concurrent Input"
+    [ let in' = [0 .. 128] :: [Int]
+          out = map Just in' <> [Nothing]
+          run = runFinal @IO . asyncToIOFinal . resourceToIOFinal . interpretRace . interpretMaskFinal . embedToFinal @IO . evalState [] . interpretLockReentrant
+          runChild = evalState @Int 0 . runInputList in' . (fmap fst . runOutputList)
+          go = whileJustM (inputConc >>= \i -> output i >> pure (void i))
+       in testCase "inputConc" . run $ do
+            threads <- replicateM 128 . async $ do
+              actualOut <- runChild go
+              embedFinal @IO $ actualOut @?= out
+            mapM_ await threads
+    ]
+
 tests :: TestTree
-tests = testGroup "Unit Tests" [testR2]
+tests = testGroup "Unit Tests" [testR2, testConcInput]
 
 main :: IO ()
 main = defaultMain tests
