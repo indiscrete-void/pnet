0f6b3e096f4e562b7b737a2ca827306fd8ac3828
indiscrete_void <indiscrete.void@gmail.com>
2024-06-16 18:31:36 +0200

    unit test runR2

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,27 +1,42 @@
 import Control.Monad
 import Control.Monad.Extra
+import Data.List qualified as List
+import Data.Maybe
 import Pnet.Routing
+import Polysemy
 import Polysemy.Async
 import Polysemy.Conc
 import Polysemy.Conc.Input
-import Polysemy.Final
-import Polysemy.Input
-import Polysemy.Output
 import Polysemy.Resource
 import Polysemy.State
+import Polysemy.Transport
 import Test.Tasty
 import Test.Tasty.HUnit
 
 data SendTo a = SendTo Node a deriving stock (Eq, Show)
 
+runInput :: i -> InterpreterFor (InputWithEOF i) r
+runInput = runInputList . List.singleton
+
+runOutput :: Sem (Output o ': r) a -> Sem r o
+runOutput = fmap (head . fst) . runOutputList
+
 testR2 :: TestTree
 testR2 =
   testGroup
     "r2"
-    [ let msg = Just "hello, world!"
-       in testCase "r2 SendTo node0 (RouteTo nb msg) = SendTo node1 (RoutedFrom na msg)" $
-            r2 SendTo 0 (RouteTo 1 msg) @?= SendTo 1 (RoutedFrom 0 msg)
+    [ testCase "r2 SendTo node0 (RouteTo nb msg) = SendTo node1 (RoutedFrom na msg)" $
+        r2 SendTo 0 (RouteTo 1 msg) @?= SendTo 1 (RoutedFrom 0 msg),
+      let runTest i = run . runOutput . runInput i . runR2 node
+       in testCase "runTest (RoutedFrom node msg) (runR2 node cat) == RouteTo node msg" $
+            runTest (RoutedFrom node msg) (input >>= output . fromJust) @?= RouteTo node msg,
+      let runTest = run . runOutput . runInputList []
+       in testCase "runTest (runR2 node close) == RouteTo node Nothing" $
+            runTest (runR2 node close) @?= RouteTo node Nothing
     ]
+  where
+    msg = Just "hello, world!"
+    node = 0
 
 testConcInput :: TestTree
 testConcInput =
@@ -29,10 +44,10 @@ testConcInput =
     "Concurrent Input"
     [ let in' = [0 .. 128] :: [Int]
           out = map Just in' <> [Nothing]
-          run = runFinal @IO . asyncToIOFinal . resourceToIOFinal . interpretRace . interpretMaskFinal . embedToFinal @IO . evalState [] . interpretLockReentrant
+          runTest = runFinal @IO . asyncToIOFinal . resourceToIOFinal . interpretRace . interpretMaskFinal . embedToFinal @IO . evalState [] . interpretLockReentrant
           runChild = evalState @Int 0 . runInputList in' . (fmap fst . runOutputList)
           go = whileJustM (inputConc >>= \i -> output i >> pure (void i))
-       in testCase "inputConc" . run $ do
+       in testCase "inputConc" . runTest $ do
             threads <- replicateM 128 . async $ do
               actualOut <- runChild go
               embedFinal @IO $ actualOut @?= out
