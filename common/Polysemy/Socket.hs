dec870f102c00059f12563bc33f47cd83cbc05ed
indiscrete_void <indiscrete.void@gmail.com>
2024-10-03 13:42:17 +0300

    trace socket io

--- common/Polysemy/Socket.hs
+++ common/Polysemy/Socket.hs
@@ -4,14 +4,23 @@ import Control.Monad
 import Network.Socket qualified as IO
 import Network.Socket.ByteString qualified as IO
 import Polysemy
+import Polysemy.Extra.Trace
+import Polysemy.Trace
 import Polysemy.Transport
 import Transport.Maybe
 
-inputToSocket :: (Member (Embed IO) r) => Int -> IO.Socket -> InterpreterFor ByteInputWithEOF r
-inputToSocket bufferSize s = interpret \Input -> embed $ eofToNothing <$> IO.recv s bufferSize
+inputToSocket :: (Member (Embed IO) r, Member Trace r) => Int -> IO.Socket -> InterpreterFor ByteInputWithEOF r
+inputToSocket bufferSize s = traceTagged "io" . go . raiseUnder @Trace
+  where
+    go = interpret \Input -> do
+      str <- embed (eofToNothing <$> IO.recv s bufferSize)
+      trace $ show str
+      pure str
 
-outputToSocket :: (Member (Embed IO) r) => IO.Socket -> InterpreterFor ByteOutput r
-outputToSocket s = interpret \(Output str) -> embed . void $ IO.send s str
+outputToSocket :: (Member (Embed IO) r, Member Trace r) => IO.Socket -> InterpreterFor ByteOutput r
+outputToSocket s = traceTagged "io" . go . raiseUnder @Trace
+  where
+    go = interpret \(Output str) -> void $ trace (show str) >> embed (IO.send s str)
 
 closeToSocket :: (Member (Embed IO) r) => Int -> IO.Socket -> InterpreterFor Close r
 closeToSocket timeout s = interpret \Close -> embed $ IO.gracefulClose s timeout
