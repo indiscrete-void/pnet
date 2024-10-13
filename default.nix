892ec46329f7fca01706e33bf33884726e37a667
indiscrete_void <indiscrete.void@gmail.com>
2024-10-13 04:45:20 +0300

    fetch my polysemy libraries from gitlab

--- default.nix
+++ default.nix
@@ -4,8 +4,15 @@ in
 pkgs.haskellPackages.developPackage {
   root = ./.;
   source-overrides = {
-    polysemy-transport = ../polysemy-transport;
-    polysemy-scoped-process = ../polysemy-scoped-process;
+    polysemy-transport = fetchGit {
+      url = "https://gitlab.com/indiscrete_void/polysemy-transport.git";
+      rev = "06e3b16a6391bd4f5b3ce5fbd67d2c9e25d050c8";
+    };
+
+    polysemy-scoped-process = fetchGit {
+      url = "https://gitlab.com/indiscrete_void/polysemy-scoped-process.git";
+      rev = "2a902e36c0d6b439c9ba41b28bff18a035a1db70";
+    };
   };
 
   modifier = drv:
